4c565a04ce2b14373baf0d9de2307a458bd4042e
Nitro <nitro@git.com>
2023-10-29 21:49:13 +0300

    by lection gide

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+QUESTIONS = [{
+        'id': i,
+        'title': f'Question {i}',
+        'content': f'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
+    } for i in range(10)]
+
+
+def index(request):
+    return render(request, 'index.html', {'questions': QUESTIONS})
+
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, 'question.html', {'question': item})
